86b9df6439ed6ac177a0d464feab5ef5150001be
PVforever <zzss11220362@gmail.com>
2023-07-05 17:19:31 +0800

    7/5日

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ require('dotenv').config();
 
 const express = require('express');
 const session = require('express-session');
+const MysqlStore = require('express-mysql-session')(session);
 const multer = require('multer');
+const moment = require('moment-timezone');
 // const upload = multer({dest: 'tmp_uploads/'});
 const upload = require(__dirname + '/modules/upload-img');
 
-
+const db = require(__dirname + '/modules/connect-db');
+const sessionStore = new MysqlStore({}, db);
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -16,9 +19,10 @@ app.use(session({
     saveUninitialized: false,
     resave: false,
     secret: 'IFjfijFIJIFj6a1v435a4gv354b3f4', //加密解密cookie
-    cookie:{
-        maxAge: 1200_000  //20分鐘
-    }
+    store: sessionStore,
+    // cookie:{
+        // maxAge: 1200_000  //20分鐘
+    // }
 }));
 
 //top-level middleware
@@ -28,7 +32,12 @@ app.use(express.json());
 //自訂的middleware
 app.use((req, res, next)=>{
     // 放在res.locals裡的變數會自動傳到 template
+
+    res.locals.toDateString = d=>moment(d).format('YYYY-MM-DD');
+    res.locals.toDatetimeString = d=>moment(d).format('YYYY-MM-DD HH:mm:ss');
+
     res.locals.title = 'my web';
+    res.locals.session = req.session; //讓所有的ejs 可以看到 session
     next();
 })
 
@@ -84,6 +93,7 @@ app.get(/^\/m\/09\d{2}-?\d{3}-?\d{3}$/i, (req, res) => {
 app.use(require(__dirname + '/routes/admin2'));
 //額外 外掛路徑 前方掛字串
 app.use('/admin3', require(__dirname + '/routes/admin3'));
+app.use('/ab', require(__dirname + '/routes/address-book'));
 
 app.get('/try-sess', (req,res) => {
     req.session.count = req.session.count || 0;
@@ -92,6 +102,82 @@ app.get('/try-sess', (req,res) => {
         count: req.session.count,
         session: req.session
     });
+})
+
+//使用moment-timezone填入時間格式
+app.get('/try-moment', (req, res) => {
+    const fm = "YYYY-MM-DD HH:mm:ss";
+
+    const m1 = moment();  //當下時間
+    const m2 = moment('2023-09-07');
+
+    res.json({
+        m1a: m1.format(fm),
+        m1b: m1.tz("Europe/London").format(fm),
+        m2a: m2.format(fm),
+        m2a: m2.tz("Europe/London").format(fm),
+        d1: new Date()
+    })
+})
+
+//登入
+app.get('/login', (req,res) => {
+    if(req.session.admin){
+    //如果已經是登入狀態直接跳轉到首頁
+    return res.redirect('/');    
+    }
+    res.render('login');
+})
+//登出
+app.get('/logout', (req,res) => {
+    delete req.session.admin;
+    res.redirect('/');
+})
+//驗證
+app.post('/login', upload.none(), (req,res) => {
+    const output = {
+        success: false,
+        code: 0,
+        msg: ''
+      };
+
+    const accounts = {
+    shin: {
+        pw: '234567',
+        nickname: '大明'
+    },
+    david: {
+        pw: '567890',
+        nickname: '小華'
+    },
+}
+const {account, password} = req.body;
+    if(!accounts[account]){
+        output.code = 400;
+        output.msg = '帳號錯誤';
+        return res.json(output);
+    }
+    if(accounts[account].pw === password){
+        output.success = true;
+        req.session.admin = {
+            account,
+            nickname: accounts[account].nickname
+        }
+    }else{
+        output.code = 420;
+        output.msg = '密碼錯誤';
+    }
+
+    res.json(output);
+})
+
+//抓幾筆資料顯示
+app.get('/try-db', async (req, res) => {
+    const sql = 'SELECT * FROM `address_book` LIMIT 5';
+
+    const [rows, fields] = await db.query(sql)
+
+    res.json({rows, fields});
 })
 
 //靜態內容的資料夾
---
+++ routes/address-book.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const dayjs = require('dayjs');
+const db = require(__dirname + "/../modules/connect-db");
+const router = express.Router();
+
+//每頁呈現的資料數量
+router.get('/', async (req, res)=>{
+    let page = req.query.page || 1;
+    page = parseInt(page);
+    if(page < 1){
+        return res.redirect('?page=1');
+    }
+    let rows = [];
+
+    const perPage = 20; //每頁最多幾筆
+    const sql = "SELECT COUNT(1) totalRows FROM `address_book`";
+    const [[{totalRows}]] = await db.query(sql);
+
+    const totalPages = Math.ceil(totalRows/perPage);
+    if(totalRows) {
+        if(page > totalPages) {
+            return res.redirect(`page=${totalPages}`);
+        }
+        const sql2 = `SELECT * FROM address_book ORDER BY sid ASC LIMIT ${(page-1)*perPage}, ${perPage}`;
+        [rows] = await db.query(sql2);
+        rows.forEach(i=>{
+            i.birthday = dayjs(i.birthday).format('YYYY-MM-DD');
+        })
+
+    }
+
+    // res.json({page, totalRows, totalPages, perPage, rows});
+    res.render('address-book/list', {page, totalRows, totalPages, perPage, rows});
+});
+
+module.exports = router;
